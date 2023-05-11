4da201c31bc8a6b055873fedc33da259b2b9c1a0
edohoangt <descience.thh10@gmail.com>
2023-05-11 14:13:36 +0700

    add react boilerplate for extension dev

---
+++ react-chrome-ext-boilerplate/src/options/options.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./options.css";
+
+const test = <p>Hello world</p>;
+
+const root = document.createElement("div");
+document.body.appendChild(root);
+
+ReactDOM.render(test, root);

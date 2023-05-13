e33e5bf429a2e4a31fc9ed7a6897b754229e1c25
edohoangt <descience.thh10@gmail.com>
2023-05-13 12:24:06 +0700

    migrate ad-blocker to manifest v3

--- simple-adblock/src/contentScript/contentScript.ts
+++ simple-adblock/src/contentScript/contentScript.ts
@@ -1,3 +1,25 @@
-chrome.runtime.sendMessage("from the content script", (response) => {
-  console.log(response);
-});
+const rules: {
+  [url: string]: () => void;
+} = {
+  "https://www.nytimes.com/section/technology": filterNYCTechnology2,
+};
+
+function filterNYCTechnology() {
+  const app = document.getElementById("site-content");
+  const wrapper = document.getElementById("top-wrapper");
+
+  app.removeChild(wrapper);
+}
+
+function filterNYCTechnology2() {
+  const divs = document.getElementsByTagName("div");
+  for (const div of divs) {
+    if (div.className.indexOf("ad") != -1) {
+      div.style.display = "none";
+    }
+  }
+}
+
+if (document.URL in rules) {
+  rules[document.URL]();
+}
